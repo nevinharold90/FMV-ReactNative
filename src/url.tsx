c096e5bdd06bf5b4e47c0c8a45deaed2fc755e92
Nevin Harold D. Cabarrubias <cabarrubias.nevinharold@gmail.com>
2025-01-30 11:35:16 +0800

    Update url.tsx

--- src/url.tsx
+++ src/url.tsx
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://192.168.62.82:3000';
 // export const API_URL: string = 'https://fmv.renzseger.com';
-export const API_URL: string = 'http://192.168.1.13:3000';       
+export const API_URL: string = 'http://192.168.139.209:3000';
